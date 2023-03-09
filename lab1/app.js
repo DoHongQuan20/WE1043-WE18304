8f8b0bb98a80917da0f8ecd7185fe8064d938851
Do Hong Quan <quandhpd08005@fpt.edu.vn>
2023-03-09 22:18:07 +0700

    nop lab1

--- lab1/app.js
+++ lab1/app.js
@@ -2,7 +2,7 @@
 let a =prompt("Nhap so nguyen a");
 //nhap b tu ban phim
 let b =prompt("Nhap so nguyen b");
-
+//ep kieu nguyen cho c
 let c = parseInt (a)+ parseInt (b);
-document.write(c);
-console.log(c);
+document.write("tong cua hai so a va b la:"+c);
+console.log("tong cua hai so a va b la:"+c);
